bad546d4ffd69167f890b80b3c114f172aab2497
Vladimir Tryasunov <tryasunov933@gmail.com>
2024-09-10 12:23:51 +0300

    add js tasks

--- js/task-1.js
+++ js/task-1.js
@@ -1,18 +1,16 @@
-function slugify(title) {
-  const titleLowerCase = title.toLowerCase();
-  const words = titleLowerCase.split(" ");
-  const slug = words.join("-");
-  return slug;
+function isEnoughCapacity(products, containerSize) {
+  const quantites = Object.values(products);
+  const totalQuantity = quantites.reduce(
+    (total, quantity) => total + quantity,
+    0
+  );
+  return totalQuantity <= containerSize;
 }
-console.log(slugify("Arrays for beginners")); // "arrays-for-beginners"
-console.log(slugify("English for developer")); // "english-for-developer"
-console.log(slugify("Ten secrets of JavaScript")); // "ten-secrets-of-javascript"
-console.log(slugify("How to become a JUNIOR developer in TWO WEEKS")); // "how-to-become-a-junior-developer-in-two-weeks"
 
-// Напиши функцію slugify(title), яка приймає заголовок статті, параметр title і
-// повертає slug, створений із цього рядка.
-// Значенням параметра title будуть рядки, слова яких розділені лише пробілами.
-// Усі символи slug повинні бути в нижньому регістрі.
-// Усі слова slug повинні бути розділені тире.
-// Візьми код нижче і встав після оголошення своєї функції для перевірки
-// коректності її роботи.У консоль будуть виведені результати її роботи.
+console.log(isEnoughCapacity({ apples: 2, grapes: 3, carrots: 1 }, 8)); // true
+
+console.log(isEnoughCapacity({ apples: 4, grapes: 6, lime: 16 }, 12)); // false
+
+console.log(isEnoughCapacity({ apples: 1, lime: 5, tomatoes: 3 }, 14)); // true
+
+console.log(isEnoughCapacity({ apples: 18, potatoes: 5, oranges: 2 }, 7)); // false
--- js/task-2.js
+++ js/task-2.js
@@ -1,24 +1,33 @@
-function makeArray(firstArray, secondArray, maxLength) {
-  const combinedArray = firstArray.concat(secondArray);
-  if (combinedArray.length > maxLength) {
-    return combinedArray.slice(0, maxLength);
+function calcAverageCalories(days) {
+  if (days.length === 0) {
+    return 0;
   }
-  return combinedArray;
+  const totalCalories = days.reduce((total, day) => total + day.calories, 0);
+  return totalCalories / days.length;
 }
-console.log(makeArray(["Mango", "Poly"], ["Ajax", "Chelsea"], 3)); // ["Mango", "Poly", "Ajax"]
-console.log(makeArray(["Mango", "Poly", "Houston"], ["Ajax", "Chelsea"], 4)); // ["Mango", "Poly", "Houston", "Ajax"]
-console.log(makeArray(["Mango"], ["Ajax", "Chelsea", "Poly", "Houston"], 3)); // ["Mango", "Ajax", "Chelsea"]
-console.log(makeArray(["Earth", "Jupiter"], ["Neptune", "Uranus"], 2)); // ["Earth", "Jupiter"]
-console.log(makeArray(["Earth", "Jupiter"], ["Neptune", "Uranus"], 4)); // ["Earth", "Jupiter", "Neptune", "Uranus"]
-console.log(makeArray(["Earth", "Jupiter"], ["Neptune", "Uranus", "Venus"], 0)); // []
 
-// Напиши функцію під назвою makeArray, яка приймає три параметри: firstArray
-// (масив), secondArray(масив) і maxLength(число).
-// Функція повинна створювати новий масив, який містить усі елементи з firstArray,
-//  а потім усі елементи з secondArray.
-// Якщо кількість елементів у новому масиві перевищує maxLength,
-// функція повинна повернути копію масиву з довжиною maxLength елементів.
-// В іншому випадку функція повинна повернути весь новий масив.
+console.log(
+  calcAverageCalories([
+    { day: "monday", calories: 3010 },
+    { day: "tuesday", calories: 3200 },
+    { day: "wednesday", calories: 3120 },
+    { day: "thursday", calories: 2900 },
+    { day: "friday", calories: 3450 },
+    { day: "saturday", calories: 3280 },
+    { day: "sunday", calories: 3300 },
+  ])
+); // 3180
 
-// Візьми код нижче і встав після оголошення своєї функції для
-// перевірки коректності її роботи.У консоль будуть виведені результати її роботи.
+console.log(
+  calcAverageCalories([
+    { day: "monday", calories: 2040 },
+    { day: "tuesday", calories: 2270 },
+    { day: "wednesday", calories: 2420 },
+    { day: "thursday", calories: 1900 },
+    { day: "friday", calories: 2370 },
+    { day: "saturday", calories: 2280 },
+    { day: "sunday", calories: 2610 },
+  ])
+); // 2270
+
+console.log(calcAverageCalories([])); // 0
